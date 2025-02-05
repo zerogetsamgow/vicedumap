1a4fb8edd3156593f7a005eed00cf2422e5a68fd
Sam Gow (Health) <sam.gow@health.vic.gov.au>
2025-02-05 21:24:29 +1100

    Version 1.0.0
    Fixes missing LGA issue.

--- data-raw/vic_edu_regions.R
+++ data-raw/vic_edu_regions.R
@@ -15,7 +15,7 @@ area.text =
   rvest::html_elements("td strong") |>
   rvest::html_text() |>
   tibble::as_tibble_col("area_name") |>
-  dplyr::filter(!str_detect(area_name,"Offices"))
+  dplyr::filter(!stringr::str_detect(area_name,"Offices"))
 
 
 
@@ -27,28 +27,28 @@ region_tbl =
   tibble::as_tibble_col() |>
   # First get region names, which all contain "Victoria"
   dplyr::mutate(
-   region_name = str_extract(value, ".*Victoria")
+   region_name = stringr::str_extract(value, ".*Victoria")
    ) |>
   tidyr::fill(region_name) |>
-  dplyr::filter(!str_detect(value,region_name)) |>
+  dplyr::filter(!stringr::str_detect(value,region_name)) |>
   # Then get LGA names, which are lists with commas
   dplyr::mutate(
-   lga_name = str_extract(value,".*,.*"),
-   lga_name = str_remove_all(lga_name,"Offices")) |>
+   lga_name = stringr::str_extract(value,".*,.*"),
+   lga_name = stringr::str_remove_all(lga_name,"Offices")) |>
   # Then get area names
   dplyr::mutate(
-   area_name = str_extract(lga_name,str_flatten(area.text$area_name,"|")),
-   area_name = coalesce(area_name,value)
+   area_name = stringr::str_extract(lga_name,stringr::str_flatten(area.text$area_name,"|")),
+   area_name = dplyr::coalesce(area_name,value)
    ) |>
   # Tidy data
   dplyr::filter(area_name!=lga_name|is.na(lga_name)) |>
-  dplyr::mutate(area_name = lag(area_name))  |>
+  dplyr::mutate(area_name = dplyr::lag(area_name))  |>
   dplyr::filter(!is.na(lga_name)) |>
   dplyr::select(contains("name")) |>
-  dplyr::mutate_all(\(x) str_remove_all(x,"\t")) |>
-  dplyr::mutate(lga_name=str_split(lga_name,", ")) |>
+  dplyr::mutate_all(\(x) stringr::str_remove_all(x,"\t")) |>
+  dplyr::mutate(lga_name = stringr::str_split(lga_name,", ")) |>
   tidyr::unnest(lga_name) |>
-  dplyr::mutate(lga_name=  str_remove(lga_name, str_flatten(area.text$area_name,"|"))) |>
+  dplyr::mutate(lga_name=  stringr::str_remove(lga_name, stringr::str_flatten(area.text$area_name,"|"))) |>
   dplyr::select(region_name, area_name, lga_name)
 
 usethis::use_data(region_tbl, overwrite = TRUE)
@@ -58,11 +58,12 @@ lga_map =
   strayr::read_absmap(
     "lga2021",
     remove_year_suffix = TRUE) |>
+  tibble::as_tibble() |>
   dplyr::filter(
     state_code == 2) |>
   dplyr::mutate(
-    lga_name = str_remove(lga_name,"\\s\\(.*\\)"),
-    lga_name = str_replace(lga_name,"Moreland","Merri-bek"))
+    lga_name = stringr::str_remove(lga_name,"\\s\\(.*\\)"),
+    lga_name = stringr::str_replace(lga_name,"Moreland","Merri-bek"))
 
 # Create region_map
 region_map =
